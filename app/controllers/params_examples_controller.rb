834fb3a052ebd528521c01f320d426b056f6e2ee
dani <dani.zaghian@gmail.com>
2021-05-06 11:06:37 -0700

    adds query params

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -1,2 +1,6 @@
 class ParamsExamplesController < ApplicationController
+  def query_params
+    my_message = params[:my_message]
+    render json: {message: "The message is #{my_message}"}
+  end
 end
